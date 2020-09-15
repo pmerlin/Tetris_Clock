52bab5ee9c128900bd5f9ebf08f0be58027d4b28
pmerlin <patrick@merlins.org>
2020-09-15 22:43:49 +0200

    init commit

---
+++ src/definitions.h
@@ -0,0 +1,31 @@
+// *********************************************************************
+// Holds all central definitions and globals
+// *********************************************************************
+
+Ticker display_ticker;          // Ticker that triggers redraw of the screen
+Ticker timer_ticker;            // Ticker that triggers one revolution in the falling animation
+
+// NTP
+int8_t timeZone = 1;            // Timezone for NTP client
+int8_t minutesTimeZone = 0;     // Minutes difference for time zone
+
+// Globals
+uint8_t init_state = 0;         // Hold the current state of the initialization process
+String str_display_time = "0000"; // Holds the currently displayed time as string
+bool seconds_odd = false;       // True when the seconds are currently odd
+
+// WiFiManager
+WiFiManager wifiManager;        // Global WiFiManager object
+#define AP_NAME "Tetris"
+#define AP_PASS "cafebabe12"
+#define AP_TIMEOUT 300
+
+
+// Pins for LED MATRIX
+#define P_LAT 16
+#define P_A 5
+#define P_B 4
+#define P_C 15
+#define P_D 12
+#define P_OE 2
+PxMATRIX display(32,32, P_LAT, P_OE,P_A,P_B,P_C,P_D);
